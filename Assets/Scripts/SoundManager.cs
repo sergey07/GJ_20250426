508be0661e11ad17b6c36dc7ea4771eda0ba28d0
Sergey <kondor07@mail.ru>
2025-04-25 17:56:43 +0300

    SoundManager

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static SoundManager Instance { get; private set; }
+
+    [SerializeField] private AudioClip _audioClip_FinishLevel;
+    [SerializeField] private AudioClip _audioClip_GameOver;
+
+    private AudioSource _audioSource;
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            transform.parent = null;
+            _audioSource = GetComponent<AudioSource>();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayFinishLevel()
     {
-        
+        _audioSource.PlayOneShot(_audioClip_FinishLevel);
+    }
+
+    public void PlayGameOver()
+    {
+        _audioSource.PlayOneShot(_audioClip_GameOver);
     }
 }
