60033091928179c92c96478e3de411155a9c6e8b
Sergey <kondor07@mail.ru>
2025-04-26 16:46:08 +0300

    Next

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,20 +1,30 @@
+using System.Collections;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    [SerializeField] private float _pauseBeforeNextLevel = 5.0f;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
-            if (LevelToggler.Instance.IsLastLevel())
-            {
-                GameManager.Instance.ShowFinishGamePanel();
-            }
-            else
-            {
-                GameManager.Instance.ShowFinishLevelPanel();
-            }
+            StartCoroutine(ShowPanel());
+        }
+    }
+
+    IEnumerator ShowPanel()
+    {
+        yield return new WaitForSeconds(_pauseBeforeNextLevel);
+
+        if (LevelToggler.Instance.IsLastLevel())
+        {
+            GameManager.Instance.ShowFinishGamePanel();
+        }
+        else
+        {
+            GameManager.Instance.ShowFinishLevelPanel();
         }
     }
 }
