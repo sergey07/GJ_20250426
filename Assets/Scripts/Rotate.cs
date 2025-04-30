daee759cc62752fb39ce3373a5b4c8b935ba407c
Sergey <kondor07@mail.ru>
2025-04-30 18:43:49 +0300

    Clock rotatation moved into FixedUpdate method

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -4,12 +4,13 @@ public class Rotate : MonoBehaviour
 {
     [SerializeField] private float _speed = 50.0f;
     [SerializeField] private GameObject _requiredPart;
-    private void Update()
+
+    private void FixedUpdate()
     {
         if (!GameManager.Instance.IsPaused() && _requiredPart.GetComponent<Rigidbody>().isKinematic == false)
         {
             float rot = Input.GetAxis("Horizontal");
-            transform.Rotate(Vector3.up, rot * _speed * Time.deltaTime);
+            transform.Rotate(Vector3.up, rot * _speed * Time.fixedDeltaTime);
         }
     }
 }
